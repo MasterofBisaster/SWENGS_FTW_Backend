cf425a56d1f9630c48c2d396ed8298a69c30c645
Urb4nOutl4w <tim.braunauer@edu.fh-joanneum.at>
2020-01-07 16:56:44 +0100

    registered adminModels for every Model

--- backend/ftw/admin.py
+++ backend/ftw/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend.ftw.models import *
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    pass
+
+
+class LocationAdmin(admin.ModelAdmin):
+
+    pass
+
+
+class EventAdmin(admin.ModelAdmin):
+
+    pass
+
+
+class FTWUserAdmin(admin.ModelAdmin):
+
+    pass
+
+
+class CommentAdmin(admin.ModelAdmin):
+
+    pass
+
+
+class FTWWordAdmin(admin.ModelAdmin):
+
+    pass
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(FTWUser, FTWUserAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(FTWWord, FTWWordAdmin)
