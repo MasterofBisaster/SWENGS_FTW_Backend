dc5e8176bbbfad73df8e6f24699af51a774aa1d4
Urb4nOutl4w <45825633+Urb4nOutl4w@users.noreply.github.com>
2020-01-16 17:13:19 +0100

    added event/id/detail to urls and its serializer and view

--- backend/ftw/serializers.py
+++ backend/ftw/serializers.py
@@ -17,38 +17,53 @@ class EventListSerializer(serializers.ModelSerializer):
     def get_category_name(self, obj):
         return obj.category.title if obj.category else ''
 
-class EventFormSerializer(serializers.ModelSerializer):
 
+class EventFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
 
-class CommentFormSerializer(serializers.ModelSerializer):
 
+class EventDetailSerializer(serializers.ModelSerializer):
+    user_name = serializers.SerializerMethodField()
+    category_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Event
+        fields = ['__all__', 'user_name', 'category_name']
+
+    def get_user_name(self, obj):
+        return obj.creator.username if obj.creator else ''
+
+    def get_category_name(self, obj):
+        return obj.category.title if obj.category else ''
+
+
+class CommentFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
 
-class CategoryFormSerializer(serializers.ModelSerializer):
 
+class CategoryFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
-class LocationFormSerializer(serializers.ModelSerializer):
 
+class LocationFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = '__all__'
 
-class FTWWordFormSerializer(serializers.ModelSerializer):
 
+class FTWWordFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = FTWWord
         fields = '__all__'
 
-class MediaSerializer(serializers.ModelSerializer):
 
+class MediaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Media
         fields = '__all__'
