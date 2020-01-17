d06fee56f9a9ba7ed2f2c30da0e32c3eba137211
MasterofBisaster <40423782+MasterofBisaster@users.noreply.github.com>
2020-01-17 12:02:10 +0100

    backend-picture&location-additions; changed picture to foreignkey; added location to eventdetail-serializer; added city to Location;

--- backend/ftw/serializers.py
+++ backend/ftw/serializers.py
@@ -27,16 +27,48 @@ class EventFormSerializer(serializers.ModelSerializer):
 class EventDetailSerializer(serializers.ModelSerializer):
     user_name = serializers.SerializerMethodField()
     category_name = serializers.SerializerMethodField()
+    location_name = serializers.SerializerMethodField()
+    location_city = serializers.SerializerMethodField()
+    location_street = serializers.SerializerMethodField()
+    location_zip_code = serializers.SerializerMethodField()
+    location_country = serializers.SerializerMethodField()
+    location_max_user = serializers.SerializerMethodField()
+
+    # location_picture = serializers.SerializerMethodField()
 
     class Meta:
         model = Event
-        fields = ['__all__', 'user_name', 'category_name']
+        fields = ['id', 'name', 'start_date', 'end_date', 'user_name', 'creator',
+                  'private', 'short_description', 'location_name', 'location_street', 'location_city',
+                  'location_zip_code', 'location_country', 'location_max_user', 'max_users', 'costs', 'user_name',
+                  'category_name']
 
     def get_user_name(self, obj):
         return obj.creator.username if obj.creator else ''
 
     def get_category_name(self, obj):
         return obj.category.title if obj.category else ''
+
+    def get_location_name(self, obj):
+        return obj.location.name if obj.location else ''
+
+    def get_location_street(self, obj):
+        return obj.location.street if obj.location else ''
+
+    def get_location_city(self, obj):
+        return obj.location.city if obj.location else ''
+
+    def get_location_zip_code(self, obj):
+        return obj.location.zip_code if obj.location else ''
+
+    def get_location_country(self, obj):
+        return obj.location.country if obj.location else ''
+
+    def get_location_max_user(self, obj):
+        return obj.location.max_user if obj.location else ''
+
+    # def get_location_picture(self, obj):
+    #   return obj.location.picture if obj.location else ''
 
 
 class CommentFormSerializer(serializers.ModelSerializer):
